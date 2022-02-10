a8901d8ed706e4ed94720b649e82c1fbf7cc63b3
max1213 <maks_serebryakov_2000@mail.ru>
2022-02-10 15:57:03 +0300

    release v1

---
+++ add/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <input type="search" v-model="search" class="search" placeholder="куда..."/>
+        <ul >
+            
+            <li v-for="item in searchHendler" :key="item.id">
+                <p>из {{item.departure_city}}</p>
+                <p>в {{item.arrival_city}}</p>
+                <p>с {{item.departure_date_time}}</p>
+                <p>по {{item.arrival_date_time}}</p>
+                <p>цена {{item.sele}}</p>
+                
+            </li>
+            
+        </ul>
+    </div>
+</template>
+<script>
+import {bilet} from './data/data';
+export default{
+    name:"App",
+    data() {
+        return{
+            search:'',
+            data:[]
+        }
+    },
+    created() {
+        this.data = bilet;
+    },
+    computed: {
+        searchHendler(){
+        return this.data.filter(elem => {
+            return elem.arrival_city.toLowerCase().includes(this.search);
+        });
+        }
+    }
+};
+</script>
+<style>
+    
+</style>
